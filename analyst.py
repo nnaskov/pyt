45d8a93fb162377309751cb03c3e407acb31e72b
Norbert Naskov <norbert.naskov@loopup.com>
2018-08-22 17:54:18 -0700

    Added longs and shorts in the price mover

--- analyst.py
+++ analyst.py
@@ -13,25 +13,47 @@ class StockHolding:
     def totalReturn(self, currentPrice):
         return (currentPrice - self.avgCost) * self.amount
 
+    def totalEquity(self, currentPrice):
+        return self.amount * currentPrice
+
+    def totalCost(self):
+        return self.amount * self.avgCost
+
+
 class Analyst:
     def __init__(self, startingCash):
+        self.startingCash = startingCash
         self.cash = startingCash
         self.shorts = StockHolding()
         self.longs = StockHolding()
 
-    def buyLong(self, stockPrice, amount):
+    def buy(self, stockPrice, amount, stock):
         if self.cash >= stockPrice * amount:
-            self.longs.addPurchases(stockPrice, amount)
+            stock.addPurchases(stockPrice, amount)
             self.cash -= stockPrice * amount
             return True
         else:
             return False
 
-    def sellLong(self, stockPrice, amount):
-        if self.longs.amount >= amount:
-            self.longs.removePurchases(amount)
+    def sell(self, stockPrice, amount, stock):
+        if stock.amount >= amount:
+            stock.removePurchases(amount)
             self.cash += stockPrice * amount
             return True
         else:
             return False
 
+    def buyLong(self, stockPrice, amount):
+      return self.buy(stockPrice, amount, self.longs)
+
+    def sellLong(self, stockPrice, amount):
+        return self.sell(stockPrice, amount, self.longs)
+
+    def buyShort(self, stockPrice, amount):
+      return self.buy(stockPrice, amount, self.shorts)
+
+    def sellShort(self, stockPrice, amount):
+        return self.sell(stockPrice, amount, self.shorts)
+
+    # This is the main algorithm
+    def act(self, currentLongPrice, currentShortPrice):
